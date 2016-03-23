de81743041c3dc3df2c92ef542d5d747eff2e4c0
Noah Rubinstein <noachr@gmail.com>
2016-03-22 21:08:19 -0700

    latest

--- client/main.js
+++ client/main.js
@@ -1,3 +1,10 @@
+import {Meteor} from "meteor/meteor"
+import {TAPi18n} from "meteor/tap:i18n"
 import "../imports/ui/components/sup"
-
+import "../imports/ui/components/langChanger"
 import "../imports/startup/client/routes"
+
+
+Meteor.startup(() =>{
+  TAPi18n.setLanguage("es");
+});
---
+++ imports/ui/components/langChanger.js
@@ -0,0 +1,17 @@
+import "./langChanger.html"
+import {TAPi18n} from "meteor/tap:i18n"
+import {Template} from "meteor/templating"
+
+Template.langChanger.events({
+  'click .langLink'(e,b){
+    console.log(this);
+    TAPi18n.setLanguage(e.currentTarget.lang);
+  }
+});
+
+Template.langChanger.helpers({
+  langList: [{code: "en", name:"English"},{code: "es", name: "Spanish"}],
+  isCurrentLang(lang){
+    return lang == TAPi18n.getLanguage();
+  }
+});
